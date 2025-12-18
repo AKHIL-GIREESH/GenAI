86bb515b907ad3869de483bbf387d37b169b6b16
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-12-18 11:22:11 +0530

    AI buddy

--- RAG for AI Buddy/main.py
+++ RAG for AI Buddy/main.py
@@ -1,12 +1,26 @@
-from langchain_ollama import ChatOllama
+from langchain_ollama import ChatOllama, OllamaEmbeddings
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from dotenv import load_dotenv
+from langchain_chroma import Chroma
 
 load_dotenv()
 model = ChatOllama(model="mistral:7b", temperature=0)
+embeddings = OllamaEmbeddings(model="mistral:7b")
+
+vector_db = Chroma(
+    collection_name="user_memory",
+    embedding_function=embeddings,
+    host="localhost",
+    port=8000
+)
+
+if vector_db._collection.count() == 0:
+    print("Adding facts to memory...")
+    facts = ["name is John Doe", "Country is India", "Is a student"]
+    vector_db.add_texts(texts=facts, ids=["fact1", "fact2", "fact3"])
 
 chatHistory = [
-    SystemMessage(content="You are this very extroverted and outgoing human friend (ESTP).")
+    SystemMessage(content="You are a human friend with personality type ESTP")
 ]
 
 while True:
@@ -14,7 +28,12 @@ while True:
     if userInput == "bye":
         break
 
-    chatHistory.append(HumanMessage(content=userInput))
+    docs = vector_db.similarity_search(userInput, k=2)
+    context = "\n".join([d.page_content for d in docs])
+
+    rag_input = f"Context from memory:\n{context}\n\nUser Question: {userInput}"
+    
+    chatHistory.append(HumanMessage(content=rag_input))
     result = model.invoke(chatHistory)
     chatHistory.append(AIMessage(content=result.content))
     print("AI:", result.content)
